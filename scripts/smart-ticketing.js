422eab82ba2b5c59cf6ce567129ce6625b2835b6
Rupam07Speed <grupamnew07@gmail.com>
2024-02-18 03:00:25 +0600

    1st function done

---
+++ scripts/smart-ticketing.js
@@ -0,0 +1,6 @@
+document.getElementById('buy-tickets').addEventListener('click', function() {
+    document.getElementById('ticket-PH-paribahan').scrollIntoView({ 
+      behavior: 'smooth' 
+    });
+  });
+  
